4d1f153515a74fc94993496d4e71041576f9846f
Cole Belog <joelkathycs@me.com>
2022-11-02 22:17:01 -0400

    Added Jacobian Calculation to Dofbot Object

--- Dofbot.py
+++ Dofbot.py
@@ -37,6 +37,7 @@ class Dofbot(RobotArm):
         self.ik_alpha = [90, 0, 0, 90, 0]  # Values of "alpha" for inverse kinematics
         self.ik_d = [self._l[0]+self._l[1], 0, 0, 0, self._l[4]+self._l[5]]  # Values of "d" for inverse kinematics
         self.ik_theta = [0, 0, -90, 0, 0]  # Values of "theta" for inverse kinematics
+        self.h = [ez, -ey, -ey, -ey, -ex]  # Values of h for Jacobian calculation
 
     def connect(self) -> bool:
         """
@@ -237,3 +238,40 @@ class Dofbot(RobotArm):
         self.setAllServoAngles(angles, moveTime)
         time.sleep(moveTime/1000)
         return True
+
+    def jacobian(self, angles: np.ndarray) -> np.matrix:
+        """
+        Calculates the arm Jacobian for the given angles
+        :param angles: The angles of the arm position
+        :return: The arm Jacobian for the given input angles
+        """
+        # Rotations in reference to base frame
+        Ri_i_1 = [np.ndarray((3, 3))] * 6
+        Ri_i_1[0] = Rotz(angles[0])
+        Ri_i_1[1] = Roty(-angles[1])
+        Ri_i_1[2] = Roty(-angles[2])
+        Ri_i_1[3] = Roty(-angles[3])
+        Ri_i_1[4] = Rotx(-angles[4])
+        Ri_i_1[5] = np.identity(3)
+        # Displacements
+        Pi_i_1 = [np.matrix([[0], [0], [0]], np.dtype(float))] * 6
+        Pi_i_1[0] = (self._l[0] + self._l[1]) * ez
+        Pi_i_1[2] = self._l[2] * ex
+        Pi_i_1[3] = -self._l[3] * ez
+        Pi_i_1[5] = -(self._l[4] + self._l[5]) * ex
+        # Base Frame Reference
+        R0i = [np.eye(3)] * 5
+        for i in range(len(R0i)):
+            for j in range(i+1):
+               R0i[i] = np.matmul(R0i[i], Ri_i_1[j])
+        P0i = [np.matrix([[0], [0], [0]], np.dtype(float))] * 6
+        P0i[0] = Pi_i_1[0]
+        for i in range(len(P0i)-1):
+            P0i[i+1] = P0i[i]+R0i[i]*Pi_i_1[i+1]
+        h0 = [np.matmul(R0i[i], self.h[i]) for i in range(len(self.h))]
+        P0T = P0i[-1]
+        cross = [np.cross(np.transpose(h0[i]), np.transpose(P0T-P0i[i])) for i in range(len(h0))]
+        J = np.matrix(np.zeros((6, 5)))
+        for i in range(5):
+            J[:,i] = np.vstack((h0[i], np.transpose(cross[i])))
+        return J
