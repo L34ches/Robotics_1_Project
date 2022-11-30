ab5c2147dd6f50da7fefdbbbd805a352a03244b8
Alapocas <sissijian0720@163.com>
2022-11-30 07:33:10 -0500

    change input to radians, add jacobian

--- FK.py
+++ FK.py
@@ -14,6 +14,13 @@ l3 = 0.08285    # servo 3 to servo 4
 l4 = 0.07385    # servo 4 to servo 5
 l5 = 0.05457    # servo 5 to gripper 
 
+# Dofbot joint axis
+h1 = ez
+h2 = -ex
+h3 = -ex
+h4 = -ex
+h5 = ey
+
 # return a rotation matrix for a radians q rotation around x-axis
 def Rx(q):
     return np.array([[1, 0, 0], 
@@ -37,24 +44,14 @@ def degToRad(q):
     return q * np.pi / 180
 
 # R0T = R01 * R12 * R23 * R34 * R45
-# q1, ..., q5 in degrees
+# q1, ..., q5 in radians
 def R0T(q1, q2, q3, q4, q5):
-    q1 = degToRad(q1)
-    q2 = degToRad(q2)
-    q3 = degToRad(q3)
-    q4 = degToRad(q4)
-    q5 = degToRad(q5)
     return np.matmul(np.matmul(sp.rot(ez, q1), sp.rot(ex, -q2 - q3 - q4)), sp.rot(ey, q5))
 
 # P12 = P45 = 0
 # P0T = P01 + R02 * P23 + R03 * P34 + R05 * P5T
-# q1, ..., q5 in degrees
+# q1, ..., q5 in radians
 def P0T(q1, q2, q3, q4, q5):
-    q1 = degToRad(q1)
-    q2 = degToRad(q2)
-    q3 = degToRad(q3)
-    q4 = degToRad(q4)
-    q5 = degToRad(q5)
     R02 = np.matmul(sp.rot(ez, q1), sp.rot(ex, -q2))
     R03 = np.matmul(R02, sp.rot(ex, -q3))
     R05 = np.matmul(np.matmul(R03, sp.rot(ex, -q4)), sp.rot(ey, q5))
@@ -63,10 +60,39 @@ def P0T(q1, q2, q3, q4, q5):
 # forward kinematics using POE method
 # returns R_0T and P_0T
 def fwkin_POE_Dofbot(q):
+    q = np.reshape(q, (-1))
     ROT = R0T(q[0], q[1], q[2], q[3], q[4])
     POT = P0T(q[0], q[1], q[2], q[3], q[4])
     return ROT, POT
 
+# q is a 5 * 1 vector of joint positions in radian
+# return Dofbot jacobian matrix given joint positions
+def jacobian(q):
+    Pi_1_i = np.concatenate(((l0+l1)*ez, np.zeros((3,1)), -l2*ey, -l3*ez, np.zeros((3,1)), (l4+l5)*ey), 1)
+    H = np.concatenate((h1, h2, h3, h4, h5), 1)
+    H_0 = np.zeros((3,5))
+    Roi = np.zeros((3,3,5))
+    Ri_1_i = np.zeros((3,3,5))
+    Poi = np.zeros((3,6))
+    for ii in range(5):
+        Ri_1_i[:,:,ii] = sp.rot(H[:,ii],q[ii])
+        if ii == 0:
+            Roi[:,:,ii] = Ri_1_i[:,:,ii]
+            Poi[:,ii] = Pi_1_i[:,ii]
+            Poi[:,ii+1] = Poi[:,ii] + np.matmul(Roi[:,:,ii], Pi_1_i[:,ii+1])
+            H_0[:,ii] = np.matmul(Roi[:,:,ii], H[:,ii])
+        else:
+            Roi[:,:,ii] = np.matmul(Roi[:,:,ii-1], Ri_1_i[:,:,ii])
+            Poi[:,ii+1] = Poi[:,ii] + np.matmul(Roi[:,:,ii], Pi_1_i[:,ii+1])
+            H_0[:,ii] = np.matmul(Roi[:,:,ii], H[:,ii])
+    Pot = Poi[:,5]
+    J = np.zeros((6,5))
+    for ii in range(5):
+        J[:,ii] = np.concatenate((H_0[:,ii], np.cross(H_0[:,ii], np.subtract(Pot, Poi[:,ii]))))
+    return J
+
 if __name__ == "__main__":
-    q = [90, 45, 135, 45, 135]
+    q = np.array([90, 45, 135, 45, 135]) * np.pi / 180
     ROT, POT = fwkin_POE_Dofbot(q)
+    jac = jacobian(q)
+    print(jac)
